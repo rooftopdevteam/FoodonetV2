8d47371784936c30caf696fd59cab4ac26d58c60
doronkapah <doronkapah@gmail.com>
2016-09-01 20:12:32 +0300

    check

--- app/src/main/java/com/example/doron/foodonetv2/MainActivity.java
+++ app/src/main/java/com/example/doron/foodonetv2/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i;
         setContentView(R.layout.activity_main);
     }
 }
